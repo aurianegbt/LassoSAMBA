597100709c39d59f1cf39fe2e82c858be6fba6fe
Auriane Gabaut <122619227+aurianegbt@users.noreply.github.com>
2023-12-18 10:34:28 +0100

    loadspace in function

--- BUILD.R
+++ BUILD.R
@@ -1,15 +1,16 @@
 # empty the memory
 rm(list=ls())
+dir <- function(x){if(!dir.exists(x)){dir.create(x)}}
 
 # Required Packages :
-suppressMessages(library(Rsmlx))
+# suppressMessages(library(Rsmlx))
+# suppressMessages(library(stats))
+# suppressMessages(library(MASS))
+# suppressMessages(library(glmnet))
+# suppressMessages(library(stringr))
+# suppressMessages(library(dplyr))
+# suppressMessages(library(slurmR))
 suppressMessages(library(lixoftConnectors))
-suppressMessages(library(stats))
-suppressMessages(library(MASS))
-suppressMessages(library(glmnet))
-suppressMessages(library(stringr))
-suppressMessages(library(dplyr))
-suppressMessages(library(slurmR))
 
 # Load Functions and softwares
 invisible(purrr::quietly(lixoftConnectors::initializeLixoftConnectors)(software='monolix', force=T))
@@ -18,12 +19,11 @@ invisible(purrr::quietly(lixoftConnectors::initializeLixoftConnectors)(software=
 arr <- as.numeric(slurmR::Slurm_env(x='SLURM_ARRAY_TASK_ID'))
 
 args = commandArgs(trailingOnly=TRUE)
+# args=c("Warfarine","reg","cov","10","FALSE","NULL","0.7")
+# project,  method,type, size, rep, weight, thresholds 
 
 project = args[1]
-
-covariateType <- args[2]
-
-buildMethod <- args[3]
+buildMethod <- args[2]
 if(buildMethod == "lassoSS"){
   buildMethod="lasso"
   stabilitySelection = TRUE
@@ -34,55 +34,48 @@ if(buildMethod == "lassoSS"){
   ncrit=20
 }else{
   stabilitySelection = FALSE
-  ncrit=100
+  ncrit=20
 }    
 
+covariateType <- args[3]
 covariateSize <- as.numeric(args[4])
 
-cluster <- as.logical(args[5])
 
-weight <- args[6]
-    if(weight=="NULL"){weight <- NULL}else{
-      Rsmlx <- paste0(Rsmlx,"PEN",weight);weight = list(covariate=as.numeric(weight))}
-
+REP = as.logical(args[5])
+weight <- if(args[6]=="NULL"){NULL}else{list(covariate=as.numeric(args[6]))}
+PEN <- !is.null(weight)
 eval(parse(text=paste0("thresholdsSS = ",args[7])))
 
-
 source("Fun/Rsmlx/fun.R")
-source("Fun/buildFS.R")
+# source("Fun/buildFS.R")
 
 ## Presentation of work :
 cat("===========================================================================\n
 - - - - - - - - - - - - - - Work Information - - - - - - - - - - - - - - -\n")
-cat(" • Method used : ")
-if(buildMethod=="reg"){cat("stepAIC")}else if(buildMethod=="lasso"){cat("lasso")}else if(buildMethod=="elasticnet"){cat("elastic net")}else{cat(buildMethod)}
-if(stabilitySelection){cat(" with stability selection")}
-if(length(thresholdsSS)!=1){cat(" using multiple thresholds")}
-cat(";\n")
-cat(" • Clusterisation : ")
-cat(cluster)
-cat(";\n")
-cat(" • Penalization : ")
-if(is.null(weight)){cat(" None")}else{cat(paste0(" ",weight))}
-cat(";\n")
-cat(paste0("Working on file n°",arr," from ",project," simulations with ",covariateSize))
-if(covariateType=="corcov"){cat(" correlated")}
-cat(" covariates.\n")
+cat(" • Method used : ",
+    if(buildMethod=="reg"){"stepAIC"}else if(buildMethod=="lasso"){"lasso"}else
+      if(buildMethod=="elasticnet"){"elastic net"}," ",
+    if(stabilitySelection){"with stability selection"}," ",
+    if(length(thresholdsSS)!=1){"using multiple thresholds"},";\n")
+cat(" • Penalization : ",if(is.null(weight)){"None"}else{weight},";\n")
+cat(" • StabSel on replicates : ",REP,";\n")
+cat("Working on file n°",arr," from ",project," simulations with ",covariateSize,if(covariateType=="corcov"){" correlated"}," covariates.\n")
 cat("===========================================================================\n")
 
 
 # Folder and paths
 pathToResults = paste0("Results/Results",project)
-if(!dir.exists(pathToResults)){dir.create(pathToResults)}
-pathToResults = paste0(pathToResults,"/")
-if(!dir.exists(pathToResults)){dir.create(pathToResults)}
-pathToResults = paste0(pathToResults,"Results_",buildMethod)
-if(stabilitySelection){pathToResults=paste0(pathToResults,"SS")}
-if(buildMethod!="reg" & length(thresholdsSS)!=1){pathToResults=paste0(pathToResults,"Crit")}
-if(cluster){pathToResults=paste0(pathToResults,"Cl")}
-if(!dir.exists(pathToResults)){dir.create(pathToResults)}
+  dir(pathToResults)
+pathToResults = paste0(pathToResults,"/Results_",buildMethod,
+                       if(stabilitySelection){"SS"},
+                       if(buildMethod!="reg" & length(thresholdsSS)!=1){"Crit"},
+                       if(PEN){paste0("PEN",paste0(weight,collapse="-"))},
+                       if(REP){"REP"})
+  dir(pathToResults)
+  
 pathToResults <- paste0(pathToResults,"/",covariateSize,covariateType)
-if(!dir.exists(pathToResults)){dir.create(pathToResults)}
+  dir(pathToResults)
+  
 pathToFolderResults = pathToResults
 pathToCompResults=paste0(pathToResults,"/compResults_",arr,".RData")
 pathToResults=paste0(pathToResults,"/buildResults_",arr,".RData")
@@ -96,7 +89,7 @@ if(dir.exists(temporaryDirectory)){
 dir.create(temporaryDirectory)
 
 pathToSim = paste0("Files/Files",project,"/",covariateSize,covariateType,"/simulation/simulation_",arr,".txt")
-
+replicatesSS=REP
 
 
 if(file.exists(pathToResults) && file.exists(pathToCompResults)){cat("Model already built.\n")}else{
@@ -110,7 +103,8 @@ if(file.exists(pathToResults) && file.exists(pathToCompResults)){cat("Model alre
                      thresholdsSS = thresholdsSS,
                      cluster=cluster,
                      weight= weight,
-                     ncrit=ncrit)
+                     ncrit=ncrit,
+                     replicatesSS=REP)
 
   model = resBuild$Model
   time = resBuild$time
