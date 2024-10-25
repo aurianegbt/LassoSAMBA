162ab5af3e58d37d53b0bdb21d973ed8d25958e3
Auriane Gabaut <122619227+aurianegbt@users.noreply.github.com>
2024-10-25 08:51:39 +0200

    add buildFS.R

---
+++ scripts/modelBuildingFun/buildFS.R
@@ -0,0 +1,51 @@
+buildFS <- function(pathToSim,
+                    project,
+                    temporaryDirectory = NULL,
+                    buildMethod = "stepAIC"){
+  
+  load(paste0("data/simulationFiles/Files",project,"/headerTypes.RData"))
+  
+  # Project initialization
+  suppressWarnings({
+    newProject(data = list(dataFile = pathToSim,
+                           headerTypes = headerTypes),
+               modelFile = paste0("data/modelFiles/",if(stringr::str_detect(project,"Pasin")){"Pasin"}else{project},".txt"))
+  })
+  
+  if(stringr::str_detect(project,"Pasin")){
+    model = "covariate"
+    
+    obs.name=getMapping()$mapping[[1]]$model
+    eval(parse(text=paste0("setErrorModel(",obs.name,"='constant')")))
+    
+    setIndividualParameterVariability(delta_S=FALSE,delta_L=FALSE)
+    setPopulationParameterInformation(
+      delta_S_pop=list(initialValue=0.23,method="FIXED"),
+      delta_L_pop=list(initialValue=0.000316,method="FIXED"))
+  }else if(project=="Naveau"){
+    model = c("covariate","correlation")
+    
+    obs.name=getMapping()$mapping[[1]]$model
+    eval(parse(text=paste0("setErrorModel(",obs.name,"='constant')")))
+    
+    setIndividualParameterVariability(D=FALSE,V=FALSE)
+    setPopulationParameterInformation(D_pop=list(initialValue=100,method="FIXED"),
+                                      V_pop=list(initialValue=30,method="FIXED"))
+    
+    setIndividualParameterDistribution(phi1="normal")
+    setIndividualParameterDistribution(phi2="normal")
+    
+  }
+  
+  saveProject(projectFile=paste0(temporaryDirectory,"/Build.mlxtran"))
+  
+  # Model Building
+  res = buildmlx(project = paste0(temporaryDirectory,"/Build.mlxtran"),
+                 buildMethod = buildMethod,
+                 model=model,
+                 test=FALSE)
+  
+  Model <- Rsmlx:::mlx.getIndividualParameterModel()
+  LL <- Rsmlx:::mlx.getEstimatedLogLikelihood()
+  return(list(Model=Model,time=res$time,iter=res$iter,LL=LL))
+}
