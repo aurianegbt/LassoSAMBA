95ee8b40be9aba04d907b86d114799cd351d19fd
Auriane Gabaut <122619227+aurianegbt@users.noreply.github.com>
2024-10-25 08:51:24 +0200

    change name to stepAIC

--- scripts/modelBuildingFun/covariateModelSelection.R
+++ scripts/modelBuildingFun/covariateModelSelection.R
@@ -18,7 +18,7 @@ covariateModelSelection <- function(buildMethod,
                                     covariate.model=NULL,
                                     criterion = "BIC",
                                     FDR_thr=0.10){
-  if(buildMethod %in% c("reg")){
+  if(buildMethod %in% c("stepAIC")){
     covariateModelSelection.reg(covFix,pen.coef,weight,n.full,nb.model,direction,paramToUse,eta,p.max,steps,sp0,iter,correlation.model)
   }else if(buildMethod=="lasso"){
     covariateModelSelection.lasso(nfolds,alpha,covFix,pen.coef,weight,paramToUse,eta,p.max,sp0,nSS,covariate.model,criterion,iter,FDR_thr)
